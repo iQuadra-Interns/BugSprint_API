deff748922168c66aeb4f4e481b0ec6272b57204
eethasatish <satish.eetha_intern@iquadra.com>
2024-07-10 00:27:26 +0530

    Update utils.py

--- applications/signin/utilities/utils.py
+++ applications/signin/utilities/utils.py
@@ -5,7 +5,8 @@ from sqlalchemy import select, and_
 import logging
 from config.database import ConnectionDetails, Tables
 from applications.signin.rq_rs.rq_signin import SignInRq
-from applications.signin.rq_rs.rs_signin import personal_details
+from applications.signin.rq_rs.rs_signin import PersonalDetails,SignInRs
+from common.classes.generic import Status, UserId
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,6 +16,7 @@ def fetch_complete_user_info(engine: Engine, sign_in_info: SignInRq):
     login_table = Table(Tables.user_login, metadata, autoload_with=engine)
 
     query_to_check_user_data = select(
+        login_table.c.id,
         login_table.c.role,
         login_table.c.category_id,
         login_table.c.email,
@@ -31,6 +33,7 @@ def fetch_complete_user_info(engine: Engine, sign_in_info: SignInRq):
 
     if not user_login_info_df.empty:
         user_info = {
+            'id' :user_login_info_df.iloc[0]['id'],
             'role': user_login_info_df.iloc[0]['role'],
             'category_id': user_login_info_df.iloc[0]['category_id'],
             'email': user_login_info_df.iloc[0]['email'],
@@ -39,8 +42,10 @@ def fetch_complete_user_info(engine: Engine, sign_in_info: SignInRq):
 
         if user_info['role'] == 'developer':
             table = Table(Tables.developer_personal_details, metadata, autoload_with=engine)
+            details_field = 'developer_details'
         elif user_info['role'] == 'tester':
             table = Table(Tables.tester_personal_detail, metadata, autoload_with=engine)
+            details_field = 'tester_details'
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid role')
 
@@ -61,14 +66,25 @@ def fetch_complete_user_info(engine: Engine, sign_in_info: SignInRq):
         logging.debug(f"Fetched details DataFrame: {details}")
 
         if not details.empty:
-            final_result = {
-                'id': details.iloc[0]['id'],
-                'first_name': details.iloc[0]['first_name'],
-                'last_name': details.iloc[0]['last_name'],
-                'phone_no': details.iloc[0]['phone_no'],
-                'role_of_the_user': details.iloc[0]['role_of_the_user']
-            }
-            return personal_details.parse_obj(final_result)
+            personal_details_obj = PersonalDetails(
+                first_name=details.iloc[0]['first_name'],
+                last_name=details.iloc[0]['last_name'],
+                email_id=user_info['email'],
+                mobile_no=details.iloc[0]['phone_no']
+            )
+            user_id_obj = UserId(
+                user_id=user_info['id'],
+                user_category=user_info['role'],
+                user_cat_id=details.iloc[0]['id']
+            )
+
+            sign_in_response = SignInRs(
+                status=Status(sts=True, err="no error", msg="Operation successful"),
+                usr=user_id_obj,
+            )
+            setattr(sign_in_response, details_field, personal_details_obj)
+
+            return sign_in_response
         else:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User details not found")
     else:
