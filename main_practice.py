5ed7369cb2b63d0e4ef6a4f2d24524f96676454e
MCS-SANJANA <sanjana.machavolu@iquadra.com>
2024-07-22 17:59:53 +0530

    edited main_practice and remoced the add_bug api i worked on

--- main_practice.py
+++ main_practice.py
@@ -7,6 +7,7 @@ os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 from fastapi import FastAPI
 from applications.add_bug.routes.routes_add import router as add_bug_router 
 from applications.bugs.routes.bugs_routes import router as bugs_list_router
+
 from applications.all.routes import all_route
 
 
@@ -16,7 +17,8 @@ app = FastAPI()
 
 app.include_router(add_bug_router, prefix="/api")
 app.include_router(bugs_list_router, prefix="/api")
-app.include_router(all_route.router, prefix="/api", tags=["common_constants"]) 
+app.include_router(all_route.router, prefix="/api", tags=["common_constants"])
+
 
 if __name__ == "__main__":
     import uvicorn
