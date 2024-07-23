1fe89ab0434ed8791604b4af97749acd0523d41f
MCS-SANJANA <sanjana.machavolu@iquadra.com>
2024-07-24 01:19:07 +0530

    remove the signin and admin from my applications

--- applications/signin/rq_rs/rq_signin.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-from typing import Union
-
-
-class SignInRq(BaseModel):
-    email: EmailStr = Field(default=...)
-    password: str = Field(default=...)
