1679ae6052d413cc198ff03b08e4322f345fbbce
MCS-SANJANA <sanjana.machavolu@iquadra.com>
2024-07-24 23:04:11 +0530

    remove db from utils folder and madde some changes created a database in config

--- applications/bugs_list/utils/utils_bugs_list.py
+++ applications/bugs_list/utils/utils_bugs_list.py
@@ -1,6 +1,26 @@
-from sqlalchemy import select
+# applications/bugs_list/utils/utils_bugs_list.py
+import sys
+import os
+
+# Add the project root directory to the Python path
+
+sys.path.append('/home/sanju/Documents/Intern_work/BugSprint_API')
+
+from sqlalchemy import Table, Column, String, select
 from sqlalchemy.exc import SQLAlchemyError
-from applications.bugs_list.utils.db import bugs_List, engine
+from config.database import engine, metadata
+
+# Define the bugs_List table
+bugs_List = Table(
+    'bugs_list', metadata,
+    Column('bug_id', String(20), primary_key=True),
+    Column('bug', String(255)),
+    Column('scenario', String(255)),
+    Column('status', String(50)),
+    Column('assignee', String(50))
+)
+
+metadata.create_all(engine)
 
 def fetch_bugs_list():
     query = select(
