4fcac7822c3af333bdc3321d4a01510dc12728fb
MCS-SANJANA <sanjana.machavolu@iquadra.com>
2024-08-02 12:31:26 +0530

    made few changes in database

--- config/database.py
+++ config/database.py
@@ -1,16 +1,17 @@
-from sqlalchemy import create_engine,MetaData
-
+from sqlalchemy import create_engine, MetaData
 class ConnectionDetails:
-    database_type ='mysql+pymysql'
-    user_name = 'satish'
-    password = 'satish123'
+    database_type = 'mysql+pymysql'
+    user_name = 'root'
+    password = 'Sanju%402004'
     host = 'localhost'
     port = '3306'
     db_default_schema_name = 'bugsprint'
 
     connection_string = f'{database_type}://{user_name}:{password}@{host}:{port}/{db_default_schema_name}'
+    print(f"Connection string: {connection_string}")
     engine = create_engine(connection_string)
-    metadata=MetaData()
+    metadata = MetaData()
+
 
 class Tables:
     USER_LOGIN_TABLE = "user_details"
@@ -18,3 +19,8 @@ class Tables:
     TESTER_PERSONAL_DETAILS = "tester_details"
     BUGS_TABLE = "bugs"
     ADMIN_PERSONAL_DETAILS = "admin_details"
+
+# Example usage:
+if __name__ == "__main__":
+    # Add your code here
+    pass
