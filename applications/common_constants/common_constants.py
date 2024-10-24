5ab956987ea8bd833e545e2d87f6ecae5a73b13b
kalyanBhaskaram <kalyanb@iquadra.com>
2024-10-24 18:30:01 +0530

    FORGOT PASSWORD

--- applications/common_constants/common_constants.py
+++ applications/common_constants/common_constants.py
@@ -9,9 +9,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from applications.common_constants.routes.all_route import router as common_constants_router
+from applications.common_constants.routes.forgot_password_router import forgot_password_router
+
 
 def add_routes(app: FastAPI):
     app.include_router(common_constants_router)
+    app.include_router(forgot_password_router)
 
 
 def configure_application() -> FastAPI:
@@ -36,6 +39,6 @@ def _():
         'sts': True,
         'err': '',
         'msg': "You've reached the common_constants application. You need to access sub routes/resources/paths"
-               
+
     }
     return resp
