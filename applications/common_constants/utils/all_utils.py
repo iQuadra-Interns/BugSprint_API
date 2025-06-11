7f6c908e90680e3390b918f15b4a35fec0c4d157
PriyaKun <keerthi.priya_intern@iquadra.com>
2025-06-11 18:47:20 +0530

    Fixed GetUserDetails
    Fixed issues in get user details method and removed warnings arose.

--- applications/common_constants/utils/all_utils.py
+++ applications/common_constants/utils/all_utils.py
@@ -4,8 +4,8 @@ from sqlalchemy.exc import SQLAlchemyError
 from config.database import DatabaseDetails, Tables,Views
 from common.classes.generic import Status
 from typing import Optional
-from applications.common_constants.rq_rs.rs_all import GetTableDataResponse
-from typing import List, Dict
+from applications.common_constants.rq_rs.rs_all import GetTableDataResponse, GetUserDetailsResponse, UserDetailsResponse
+from typing import Optional, List, Dict
 from sqlalchemy import Table, MetaData, select
 
 logger = logging.getLogger(__name__)
@@ -105,24 +105,22 @@ def get_table_data(engine: Engine, table_name: Optional[str] = None):
         )
 
 
-def get_user_details(engine) -> List[Dict]:
+
+def get_user_details(engine) -> List[UserDetailsResponse]:
+    logger.info("Fetching user details from the database")
     metadata = MetaData()
     user_details_view = Table(
         Views.USER_DETAILS,
         metadata,
         autoload_with=engine
     )
-    user_types = Table(
-        "user_types",
-        metadata,
-        autoload_with=engine
-    )
-
-    query = (select(user_details_view.c.user_id, user_details_view.c.user_name, user_details_view.c.email,user_types.c.user_type_name.label("role")).
-             select_from(user_details_view.join(user_types,user_details_view.c.user_type == user_types.c.user_type_name)).
-             where(user_details_view.c.user_type.in_(["ADM", "DEV"])))
+    query = (
+        select(user_details_view.c.user_id, user_details_view.c.user_name, user_details_view.c.email, user_details_view.c.user_type.label("role")).
+            where(user_details_view.c.user_type.in_(["ADM", "DEV", "TES"])))
 
     with engine.connect() as connection:
-        result = connection.execute(query)
-        return [{"user_id": row.user_id, "user_name": row.user_name, "email": row.email,"role": row.role} for row in result]
-
+        result = connection.execute(query).mappings()
+        users = [
+            UserDetailsResponse(user_id=row["user_id"], user_name=row["user_name"], email=row["email"], role=row["role"]) for row in result]
+    logger.info("Fetched %d user details (including NULL user_names)", len(users))
+    return users
